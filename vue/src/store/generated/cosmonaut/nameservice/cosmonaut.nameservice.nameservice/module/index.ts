4f453c8e1e543ae10e8b2af9aaf7ee10be88289b
haiTran <transonhai0805@gmail.com>
2022-01-15 14:48:31 +0700

    starport scaffold message set-name name value

--- vue/src/store/generated/cosmonaut/nameservice/cosmonaut.nameservice.nameservice/module/index.ts
+++ vue/src/store/generated/cosmonaut/nameservice/cosmonaut.nameservice.nameservice/module/index.ts
@@ -5,10 +5,12 @@ import { SigningStargateClient } from "@cosmjs/stargate";
 import { Registry, OfflineSigner, EncodeObject, DirectSecp256k1HdWallet } from "@cosmjs/proto-signing";
 import { Api } from "./rest";
 import { MsgBuyName } from "./types/nameservice/tx";
+import { MsgSetName } from "./types/nameservice/tx";
 
 
 const types = [
   ["/cosmonaut.nameservice.nameservice.MsgBuyName", MsgBuyName],
+  ["/cosmonaut.nameservice.nameservice.MsgSetName", MsgSetName],
   
 ];
 export const MissingWalletError = new Error("wallet is required");
@@ -42,6 +44,7 @@ const txClient = async (wallet: OfflineSigner, { addr: addr }: TxClientOptions =
   return {
     signAndBroadcast: (msgs: EncodeObject[], { fee, memo }: SignAndBroadcastOptions = {fee: defaultFee, memo: ""}) => client.signAndBroadcast(address, msgs, fee,memo),
     msgBuyName: (data: MsgBuyName): EncodeObject => ({ typeUrl: "/cosmonaut.nameservice.nameservice.MsgBuyName", value: MsgBuyName.fromPartial( data ) }),
+    msgSetName: (data: MsgSetName): EncodeObject => ({ typeUrl: "/cosmonaut.nameservice.nameservice.MsgSetName", value: MsgSetName.fromPartial( data ) }),
     
   };
 };
